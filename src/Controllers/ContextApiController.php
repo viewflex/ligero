da8e2d28a7841c7a6ae31cfcb988e6672337b5e9
Adam Sailor <viewflex.dev@gmail.com>
2017-12-31 16:12:32 +0200

    ContextApiController methods refactored to HasContextApi trait.

--- src/Controllers/ContextApiController.php
+++ src/Controllers/ContextApiController.php
@@ -7,6 +7,8 @@ use Viewflex\Ligero\Contracts\ContextInterface as Context;
 
 class ContextApiController extends Controller
 {
+    use HasContextApi;
+
     /**
      * @var array
      */
@@ -26,135 +28,6 @@ class ContextApiController extends Controller
     {
         $this->inputs = json_decode(request()->getContent(), true);
         $this->contexts = config('ligero.contexts', []);
-    }
-
-    /*
-    |--------------------------------------------------------------------------
-    | JSON Publisher Query Actions
-    |--------------------------------------------------------------------------
-    */
-
-    /**
-     * Returns the results of json publisher query on id in native or array format.
-     *
-     * @param string $key
-     * @return array
-     */
-    public function find($key)
-    {
-        $this->context = new $this->contexts[$key];
-        return $this->context->find($this->inputs['id'], false);
-    }
-
-    /**
-     * Querying on json inputs provided, which may not have been validated,
-     * returns the results of publisher query in native or array format.
-     *
-     * @param string $key
-     * @return array
-     */
-    public function findBy($key)
-    {
-        $this->context = new $this->contexts[$key];
-        return $this->context->findBy($this->inputs, false);
-    }
-
-    /**
-     * Store a record using json inputs.
-     *
-     * @param string $key
-     * @return array
-     */
-    public function store($key)
-    {
-        $this->context = new $this->contexts[$key];
-        return $this->context->store($this->inputs);
-    }
-
-    /**
-     * Update a record using json inputs.
-     *
-     * @param string $key
-     * @return array
-     */
-    public function update($key)
-    {
-        $this->context = new $this->contexts[$key];
-        $this->context->update($this->inputs);
-    }
-
-    /**
-     * Delete a record using json input.
-     *
-     * @param string $key
-     * @return array
-     */
-    public function destroy($key)
-    {
-        $this->context = new $this->contexts[$key];
-        $this->context->delete($this->inputs['id']);
-    }
-
-    /*
-    |--------------------------------------------------------------------------
-    | JSON Rich Context Query Actions
-    |--------------------------------------------------------------------------
-    */
-
-    /**
-     * Returns the results of json context query on id in native or array format.
-     *
-     * @return mixed
-     */
-    public function findContext($key)
-    {
-        $this->context = new $this->contexts[$key];
-        return $this->context->findContext($this->inputs['id'], false);
-    }
-
-    /**
-     * Find by attributes using json query inputs,
-     * returns results in native or array format.
-     *
-     * @return mixed
-     */
-    public function findContextBy($key)
-    {
-        $this->context = new $this->contexts[$key];
-        return $this->context->findContextBy($this->inputs, false);
-    }
-
-    /**
-     * Store a context using json inputs.
-     *
-     * @return int
-     */
-    public function storeContext($key)
-    {
-        $this->context = new $this->contexts[$key];
-        return $this->context->storeContext($this->inputs);
-    }
-
-    /**
-     * Update a context using json inputs.
-     *
-     * @return int
-     */
-    public function updateContext($key)
-    {
-        $this->context = new $this->contexts[$key];
-        return $this->context->updateContext($this->inputs);
-    }
-
-    /**
-     * Delete a context using json input.
-     *
-     * @return int
-     */
-    public function destroyContext($key)
-    {
-        $this->context = new $this->contexts[$key];
-        return $this->context->deleteContext($this->inputs['id']);
     }
     
 }
---
+++ src/Controllers/HasContextApi.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace Viewflex\Ligero\Controllers;
+
+trait HasContextApi
+{
+    /*
+    |--------------------------------------------------------------------------
+    | JSON Publisher Query Actions
+    |--------------------------------------------------------------------------
+    */
+
+    /**
+     * Returns the results of json publisher query on id.
+     *
+     * @param string $key
+     * @return array
+     */
+    public function find($key)
+    {
+        $this->context = new $this->contexts[$key];
+        return $this->context->find($this->inputs['id'], false);
+    }
+
+    /**
+     * Returns the results of json publisher query.
+     *
+     * @param string $key
+     * @return array
+     */
+    public function findBy($key)
+    {
+        $this->context = new $this->contexts[$key];
+        return $this->context->findBy($this->inputs, false);
+    }
+
+    /**
+     * Store a record using json inputs.
+     *
+     * @param string $key
+     * @return array
+     */
+    public function store($key)
+    {
+        $this->context = new $this->contexts[$key];
+        return $this->context->store($this->inputs);
+    }
+
+    /**
+     * Update a record using json inputs.
+     *
+     * @param string $key
+     * @return array
+     */
+    public function update($key)
+    {
+        $this->context = new $this->contexts[$key];
+        $this->context->update($this->inputs);
+    }
+
+    /**
+     * Delete a record using json input.
+     *
+     * @param string $key
+     * @return array
+     */
+    public function destroy($key)
+    {
+        $this->context = new $this->contexts[$key];
+        $this->context->delete($this->inputs['id']);
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | JSON Rich Context Query Actions
+    |--------------------------------------------------------------------------
+    */
+
+    /**
+     * Returns the results of json context query on id.
+     *
+     * @return mixed
+     */
+    public function findContext($key)
+    {
+        $this->context = new $this->contexts[$key];
+        return $this->context->findContext($this->inputs['id'], false);
+    }
+
+    /**
+     * Returns the results of json context query.
+     *
+     * @return mixed
+     */
+    public function findContextBy($key)
+    {
+        $this->context = new $this->contexts[$key];
+        return $this->context->findContextBy($this->inputs, false);
+    }
+
+    /**
+     * Store a context using json inputs.
+     *
+     * @return int
+     */
+    public function storeContext($key)
+    {
+        $this->context = new $this->contexts[$key];
+        return $this->context->storeContext($this->inputs);
+    }
+
+    /**
+     * Update a context using json inputs.
+     *
+     * @return int
+     */
+    public function updateContext($key)
+    {
+        $this->context = new $this->contexts[$key];
+        return $this->context->updateContext($this->inputs);
+    }
+
+    /**
+     * Delete a context using json input.
+     *
+     * @return int
+     */
+    public function destroyContext($key)
+    {
+        $this->context = new $this->contexts[$key];
+        return $this->context->deleteContext($this->inputs['id']);
+    }
+    
+}
